47ba5c12b77323742ffd6acda1ed8e6d05be858c
Juanita Samborski <juanita@breakthroughwebsite.com>
2022-03-29 15:09:19 -0700

    Toolkit section

--- src/components/Toolkit.js
+++ src/components/Toolkit.js
@@ -1,16 +1,28 @@
-import React from 'react'
-import toolkitIcons from '../data/toolkit.json'
+import React from "react";
+import toolkitIcons from "../data/toolkit.json";
 
 export default function Toolkit() {
   return (
-    <div>
+    <div className="toolkit__container">
+      <div className="row">
+        <div className="col col-3 toolkit__title">
+          <h1>Toolkit</h1>
+        </div>
+
+        <div className="col col-2 toolkit__subtitle">
+            <h3>Experience</h3>
+        </div>
+
         
-      {toolkitIcons.map(({id, altText, image}) => (
-          <div key={id}>
-            <img src={image} alt={altText} />
+          <div className="col col-7 toolkit__icon-container">
+            {toolkitIcons.map(({ id, altText, image }) => (
+              <div key={id}>
+                <img src={image} alt={altText} width="auto" height="50px" />
+              </div>
+            ))}
           </div>
-      ))}
-     
-    </div>
-  )
+        </div>
+      </div>
+   
+  );
 }
